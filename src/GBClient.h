655e6607f85749477316d1294c00d5f1a334d832
kaijungchen <kaijung.chen@gigamedia.com.tw>
2014-11-28 20:55:29 +0800

    Add GBClient's feature

--- src/GBClient.h
+++ src/GBClient.h
@@ -5,10 +5,47 @@
 //  Created by kaijung on 2014/11/12.
 //
 //
+@protocol GBClientDelegate;
+
 @interface GBClient : NSObject {
-    id<GDialogDelegate> _delegate;    
+    id<GBClientDelegate> _delegate;
 }
 
+@property (nonatomic, assign) id<GBClientDelegate> delegate;
 
+- (id)initWithGameId : (NSString*) gameId;
+- (id)login;
+- (id)callService;
+- (id)getProductList;
+- (id)purchase : (NSString*) cid serverId:(NSString*) server
+        itemID : (NSString*) item onSalesId: (NSString*) onsalesId
+     providerID: (NSString*) providerId
+          token: (NSString*) token;
 
+- (id)subPush : (NSString*) regid;
+- (id)unsubPush : (NSString*) regid;
+
+- (void)GBClientDidComplete:(NSInteger) code result:(NSString *)json;
+- (void)GBClientDidNotComplete:(NSInteger) code result:(NSString *)json;
+- (void)GBClientDidFailWithError:(NSError *)error;
+@end
+
+@protocol GBClientDelegate <NSObject>
+
+@optional
+
+/**
+ * Called when the dialog succeeds and is about to be dismissed.
+ */
+- (void)didComplete:(NSInteger) code result:(NSString *)json;
+
+/**
+ * Called when the dialog succeeds with a returning url.
+ */
+- (void)didNotComplete:(NSInteger) code result:(NSString *)json;
+
+/**
+ * Called when dialog failed to load due to an error.
+ */
+- (void)didFailWithError:(NSError *)error;
 @end
--- src/GBClient.m
+++ src/GBClient.m
@@ -7,3 +7,73 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GBClient.h"
+
+@implementation GBClient {
+
+}
+
+@synthesize delegate = _delegate;
+
+- (id)initWithGameId : (NSString*) gameId {
+    
+}
+
+- (id)login {
+    
+}
+
+- (id)callService {
+    
+}
+
+- (id)getProductList {
+    
+}
+
+- (id)purchase : (NSString*) cid serverId:(NSString*) server
+        itemID : (NSString*) item onSalesId: (NSString*) onsalesId
+     providerID: (NSString*) providerId
+          token: (NSString*) token {
+    
+}
+
+- (id)subPush : (NSString*) regid {
+    
+}
+
+- (id)unsubPush : (NSString*) regid {
+    
+}
+
+- (void)GBClientDidComplete:(NSInteger) code result:(NSString *)json {
+    // retain self for the life of this method, in case we are released by a client
+    id me = [self retain];
+    
+    @try {
+        // call into client code
+        if ([_delegate respondsToSelector:@selector(didComplete:)]) {
+            [_delegate didComplete:code result:json];
+        }
+        
+    } @finally {
+        [me release];
+    }
+}
+
+- (void)GBClientDidNotComplete:(NSInteger) code result:(NSString *)json {
+    // retain self for the life of this method, in case we are released by a client
+    id me = [self retain];
+    
+    @try {
+        // call into client code
+        if ([_delegate respondsToSelector:@selector(didNotComplete:)]) {
+            [_delegate didNotComplete:code result:json];
+        }
+        
+    } @finally {
+        [me release];
+    }
+}
+
+@end
